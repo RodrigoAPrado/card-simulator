f4578576c1fe2682398f38573257db831ad0b564
Rodrigo Almeida Prado <rodrigo.almprado@gmail.com>
2026-05-16 02:33:21 -0300

    WIP ocgcore

---
+++ YGO/Assets/Ygo/Scripts/Controller/OcgCoreManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+using Ygo.Core.Bridge;
+
+namespace Ygo.Controller
+{
+    public class OcgCoreManager : MonoBehaviour
+    {
+        public void Awake()
+        {
+            var teste = new DuelBridge();
+            //teste.Start();
+        }
+    }
+}
---
+++ YGO/Assets/Ygo/Scripts/Core/Bridge/DuelBridge.cs
@@ -0,0 +1,15 @@
+﻿namespace Ygo.Core.Bridge
+{
+    public class DuelBridge
+    {
+        public void Start()
+        {
+            OcgCoreNative.start_duel();
+        }
+
+        public void SendAction(int player, int actionType)
+        {
+            OcgCoreNative.process(player, actionType);
+        }
+    }
+}
