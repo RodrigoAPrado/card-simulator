cb5abe4a0857c5fdd2c002b1abaa8ad19b4cdb9c
Rodrigo Almeida Prado <rodrigo.almprado@gmail.com>
2026-05-23 21:53:25 -0300

    wip fieldzones

--- YGO/Assets/Ygo/Scripts/Controller/Field/MainDeckController.cs
+++ YGO/Assets/Ygo/Scripts/Controller/Field/MainDeckController.cs
@@ -3,34 +3,20 @@ using UnityEngine;
 using Ygo.Controller.Component;
 using Ygo.Scripts.Core.Enum;
 using Ygo.View;
+using Ygo.View.Field;
 
 namespace Ygo.Controller.Field
 {
     public class MainDeckController : MonoBehaviour
     {
         [field: SerializeField] 
-        private HoverController hoverController;
-        [field: SerializeField] 
-        private HighlightController highlightController;
-        [field: SerializeField] 
-        private TextViewUI textView;
+        private MainDeckView view;
         [field: SerializeField] 
         private PointOfView pointOfView;
         
-        public void Init()
+        public void Init(int deckSize)
         {
-        }
-        
-        public void OnClick()
-        {
-        }
-
-        public void OnEnter()
-        {
-        }
-
-        public void OnExit()
-        {
+            view.SetSize(deckSize);
         }
     }
 }
--- YGO/Assets/Ygo/Scripts/View/Field/MainDeckView.cs
+++ YGO/Assets/Ygo/Scripts/View/Field/MainDeckView.cs
@@ -1,7 +1,48 @@
-﻿namespace Ygo.View.Field
+﻿using UnityEngine;
+
+namespace Ygo.View.Field
 {
-    public class MainDeckView
+    public class MainDeckView : MonoBehaviour
     {
-        
+        [field: SerializeField] 
+        private GameObject[] deckGraphics;
+        [field: SerializeField] 
+        private TextViewUI deckCountText;
+
+        public void SetSize(int deckCount)
+        {
+            deckCountText.SetText(deckCount.ToString());
+            switch (deckCount)
+            {
+                case >30:
+                    ToggleDeckGraphics(3);
+                    break;
+                case >20:
+                    ToggleDeckGraphics(2);
+                    break;
+                case >10:
+                    ToggleDeckGraphics(1);
+                    break;
+                case >0:
+                    ToggleDeckGraphics(0);
+                    break;
+                default:
+                    ToggleDeckGraphics(-1);
+                    break;
+            }
+        }
+
+        private void ToggleDeckGraphics(int deckGraphicsIndex)
+        {
+            foreach (var deck in deckGraphics)
+            {
+                deck.SetActive(false);
+            }
+
+            if (deckGraphicsIndex < 0)
+                return;
+            
+            deckGraphics[deckGraphicsIndex].SetActive(true);
+        }
     }
 }
